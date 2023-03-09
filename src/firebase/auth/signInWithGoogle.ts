e6297fc7a1597ce3e7fa6895751b8c78594d297f
Joen Tan <joentze01@gmail.com>
2023-03-10 00:32:59 +0800

    Stopped tracking env.local, and env.staging

---
+++ src/firebase/auth/signInWithGoogle.ts
@@ -0,0 +1,16 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../base";
+const GoogleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async (): Promise<void> => {
+  signInWithPopup(auth, GoogleProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      //   const credential = GoogleAuthProvider.credentialFromResult(result);
+      //   const token = (credential as OAuthCredential).accessToken;
+      console.log(result.user);
+    })
+    .catch((error) => {
+      throw new Error(error.message);
+    });
+};

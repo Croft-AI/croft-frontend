059fddca7c3e94ba1347774c045712fc7d0f2a4a
Joen Tan <joentze01@gmail.com>
2023-03-11 16:09:29 +0800

    refactor: refactored userHandlers into auth

--- src/firebase/auth/userHandler.ts
+++ src/firebase/auth/userHandler.ts
@@ -1,6 +1,48 @@
-import { auth } from "../base";
+import { auth, db } from "../base";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
 
 export const logout = async (): Promise<void> => {
   signOut(auth);
+};
+
+export interface User {
+  createdOn: Date;
+  email: string;
+  impressions: string[];
+  lastLogin: Date;
+  firstName: string;
+  lastName: string;
+  photoUrl: string;
+}
+
+export const createUser = async (
+  uid: string,
+  userObject: User
+): Promise<void> => {
+  try {
+    await setDoc(doc(db, "user", uid), userObject);
+  } catch (e) {
+    throw new Error("There was a problem creating new user.");
+  }
+};
+
+export const checkUserExists = async (uid: string): Promise<boolean> => {
+  const docRef = doc(db, "user", uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) return true;
+  return false;
+};
+
+export const googleSignInHandler = async (
+  uid: string,
+  userObject: User
+): Promise<void> => {
+  try {
+    const isUserExists = await checkUserExists(uid);
+    if (isUserExists) return;
+    await createUser(uid, userObject);
+  } catch (e) {
+    throw new Error("There was a problem signing in with Google.");
+  }
 };
